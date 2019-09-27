cc3b9f7cb68992c9e3123d4a17c47685640ec1e8
Guillerbr <ghminvest@gmail.com>
2019-09-27 20:44:05 -0300

    App.js functions github access api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
 
-const Profile = ({ user }) => (
+import axios from 'axios';
+import Profile from './Profile';
+import Repo from './Repo';
 
-  <div>
-    <div className="row">
-      <div className="col-md-4">
-        <div className="card" style={{ width: '18rem' }}>
-          <img className="card-img-top" src={user.avatar_url} />
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              Repositorios:
-                                <span className="badge badge-sucess">{user.public_repos}</span>
-            </li>
-            <li className="list-group-item">
-              Seguidores:
-             <span className="badge badge-primary">{user.followers}</span>
-            </li>
-            <li className="list-group-item">
-              Seguindo:
-             <span className="badge badge-info">{user.following}</span>
-            </li>
-          </ul>
-          <div className="card-body">
-            <a href={user.html_url} className="btn btn-success btn-block">
-              Ver perfil
-                        </a>
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      github: {
+        url: "https://api.github.com/users",
+        client_id: "c4b0891f235a6ebe4eee",
+        client_secret: "aefb2e743322e58c2210efaf688fe3e2f98ea486",
+        count: 16,
+        sort: "created: asc"
+      },
+      user: [],
+      repos: []
+    };
+  }
+
+  //function
+  getUser = (e) => {
+    const user = e.target.value;
+
+    const { url, client_id, client_secret, count, sort } = this.state.github;
+    axios
+      .get(
+        `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+      )
+      .then(({ data }) => this.setState({ user: data }));
+    axios
+      .get(
+        `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&cliente_secret=${client_secret}`
+      )
+      .then(({ data }) => this.setState({ repos: data }));
+
+  };
+
+
+//function 
+  renderProfile = () => {
+
+    const { user, repos } = this.state;
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <Profile user={user} />
+        </div>
+        <div className="col-md-8">
+          {repos.map(repo => (
+            <Repo key={repo.name} repo={repo} />
+          ))}
+        </div>
+
+      </div>
+    );
+  }
+
+
+//action
+  render() {
+    return (
+      <div className="App">
+
+        <Navbar />
+
+        <div className="container">
+          <div className="card card-body">
+            <h1>Pesquisar Usuários</h1>
+            <p className="lead">Digite um nome ou repositório</p>
+            <input onChange={this.getUser} id="search" type="text" className="form-control" required />
+
 
           </div>
+
+          {this.state.user.length !== 0 ? this.renderProfile() : null}
         </div>
+
       </div>
-    </div>
-  </div>
+    );
+  }
+}
 
-);
 
-export default Profile;
+export default App;
